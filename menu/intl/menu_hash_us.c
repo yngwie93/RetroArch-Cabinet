3fa383d3970791f0ca5dea1da9f8938e59a3b09f
Rasmus Eskola <fruitiex@gmail.com>
2015-08-06 10:12:08 +0300

    clearer labels for selecting games

--- menu/intl/menu_hash_us.c
+++ menu/intl/menu_hash_us.c
@@ -1069,11 +1069,11 @@ const char *menu_hash_to_str_us(uint32_t hash)
       case MENU_LABEL_VALUE_CONTENT_COLLECTION_LIST:
          return "Select From Collection";
       case MENU_LABEL_VALUE_DETECT_CORE_LIST:
-         return "Select File And Detect Core";
+         return "Select Game";
       case MENU_LABEL_VALUE_DOWNLOADED_FILE_DETECT_CORE_LIST:
          return "Select Downloaded File And Detect Core";
       case MENU_LABEL_VALUE_LOAD_CONTENT_HISTORY:
-         return "Load Recent";
+         return "Recently Played";
       case MENU_LABEL_VALUE_AUDIO_ENABLE:
          return "Audio Enable";
       case MENU_LABEL_VALUE_FPS_SHOW:
